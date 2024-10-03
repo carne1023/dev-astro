6441c053685b51578da5da421beafbcd694ee5ee
carne1023 <canutolocomiguel@gmail.com>
2024-10-03 13:33:00 -0500

    newrepo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// @ts-nocheck
+import { defineConfig, envField } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercelServerless(),
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({
+        context: "server",
+        access: "public",
+        default: true,
+      }),
+      SCORE_API_ENDPOINT: envField.string({
+        context: "server",
+        access: "public",
+      }),
+    },
+  },
+});
